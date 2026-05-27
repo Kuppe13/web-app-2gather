09011925ad9927fb1e90e48c8ee88540ada038eb
Kuppe13 <eaa25maku@students.eaaa.dk>
2026-05-27 09:28:55 +0200

    tilføjet vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./main.css";
+import { BrowserRouter } from "react-router";
+import "./styles.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
