b57e7fc2e0cb47a958bb13c0d1998e18ebffc720
Celina25 <eaa25celu@students.eaaa.dk>
2026-05-28 11:12:04 +0200

    Tilføjelse af profile page css og jsx

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import "./ProfilePage.css";
+
+function ProfilePage() {
+  return (
+    <div className="profile-page">
+      <h1>MIN PROFILSIDE</h1>
+    </div>
+  );
+}
+
+export default ProfilePage;
