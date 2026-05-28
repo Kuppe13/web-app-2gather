fa9835236ac4bc6b675acdb8645eae738076b0d9
Celina25 <eaa25celu@students.eaaa.dk>
2026-05-28 11:37:32 +0200

    Card lavet

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,7 +3,22 @@ import "./ProfilePage.css";
 function ProfilePage() {
   return (
     <div className="profile-page">
-      <h1>MIN PROFILSIDE</h1>
+      <div className="profile-card">
+        <div className="profile-top">
+          <img
+            className="profile-image"
+            src="https://via.placeholder.com/120"
+            alt="profile"
+          />
+
+          <div className="profile-info">
+            <h2>Kübra</h2>
+            <p>@kubrafidan</p>
+
+            <button>Tilføj</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
